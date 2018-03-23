99a1ba4c94d5230ac1245e089bdc0aa794bb73e4
765914173@qq.com <765914173@qq.com>
2018-03-23 15:27:15 +0800

    2018/3/23

---
+++ Desktop/Vue框架搭建/vue_demo/src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="test">
+      {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'hello vue',
+  data(){
+      return{
+          message:'this is my first vue interface'
+      }
+  }
+}
+</script>
